d9fb7a37b2e8f45b7e5f9a22c520b6fc79a6ec06
Pierre-Olivier Cote <po.cote@outlook.com>
2020-03-21 17:05:23 -0400

    sentiment analysis

--- projet/main.py
+++ projet/main.py
@@ -14,4 +14,8 @@ if __name__ == "__main__":
     command_line_args = initialize_argparser()
     tweets_handler.authentify()
     tweets = tweets_handler.get_tweets_by_user(command_line_args.user, 200)
+    for tweet in tweets:
+        p = sentiment_analysis.get_polarity(tweet.text)
+        s = sentiment_analysis.get_subjectivity(tweet.text)
+        print(u"Polarity {}, subjectivity {}".format(p, s))
     
