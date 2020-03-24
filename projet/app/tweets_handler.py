41c2857778d7258f978969d8db3c7fcff10787c1
Pierre-Olivier Cote <po.cote@outlook.com>
2020-03-24 10:47:53 -0400

    first version tweet_handler

--- projet/app/tweets_handler.py
+++ projet/app/tweets_handler.py
@@ -1,5 +1,6 @@
 import tweepy
 from threading import Lock
+import math
 
 consumer_key = "zr67B9ROvy2PS26gzIXegiwgd"
 consumer_secret_key = "onLPhPHRfFc2KX1VF6uzK4J2Mck2pi9hAfY1vAEwbbIKn3eI8C"
@@ -9,6 +10,9 @@ access_token_secret =  "TiQKAWe9wEhiPJp1pXMbXp8CDB8JZYfNBjAhEkHar9afh"
 lock = Lock()
 api = None
 
+# constants
+MAX_TWEETS = 3200 #Twitter ne conserve que les 3200 tweets les plus récents
+REQUEST_SIZE = 200
 
 def authentify(key=consumer_key, secret_key=consumer_secret_key, token=access_token, secret_token=access_token_secret):
     global api
@@ -19,5 +23,32 @@ def authentify(key=consumer_key, secret_key=consumer_secret_key, token=access_to
     lock.release() 
 
 
-def get_tweets_by_user(user, nb):
-    return api.user_timeline(screen_name = user, count = nb)
+def get_tweets_by_user(username, nb = REQUEST_SIZE):
+    return api.user_timeline(screen_name = username, count = nb)
+
+def get_tweets_before_tweetid(username, before_id, nb = REQUEST_SIZE):
+    return api.user_timeline(screen_name = username, count = nb, max_id = before_id)
+
+def in_period_tweets(tweets, start_date, end_date):
+    r_tweets = []
+    for tweet in tweets:
+        if tweet.created_at < end_date and tweet.created_at > start_date:
+            r_tweets.append(tweet)
+    return r_tweets
+
+def are_tweets_too_old(tweets, start_date):
+    return tweets[0].created_at < start_date
+     
+
+def get_tweets_in_period(username, start_date, end_date):
+    tweets = []
+    max_request_nb = math.ceil(MAX_TWEETS/REQUEST_SIZE)
+
+    tmpTweets = get_tweets_by_user(username)
+    for _ in range(0, max_request_nb):
+        print(tmpTweets[0].created_at)
+        if are_tweets_too_old(tmpTweets, start_date):
+            break
+        tweets.extend(in_period_tweets(tmpTweets, start_date, end_date))
+        tmpTweets = get_tweets_before_tweetid(username, tmpTweets[-1].id)
+    return tweets
--- projet/main.py
+++ projet/main.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 
 from app import news_handler, sentiment_analysis, tweets_handler
 
@@ -13,7 +14,13 @@ def initialize_argparser():
 if __name__ == "__main__":
     command_line_args = initialize_argparser()
     tweets_handler.authentify()
-    tweets = tweets_handler.get_tweets_by_user(command_line_args.user, 200)
+    startDate = datetime.datetime(2018, 6, 1, 0, 0, 0)
+    endDate =   datetime.datetime(2019, 1, 1, 0, 0, 0)
+
+
+    tweets = tweets_handler.get_tweets_in_period(command_line_args.user, startDate, endDate)
+    if not tweets:
+        print("No tweet found")
     for tweet in tweets:
         p = sentiment_analysis.get_polarity(tweet.text)
         s = sentiment_analysis.get_subjectivity(tweet.text)
