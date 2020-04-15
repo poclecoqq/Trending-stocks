554c284ddfe9eda9f4e7fe97ba063fd5c8f9c888
Pierre-Olivier Cote <po.cote@outlook.com>
2020-04-15 16:07:11 -0400

    automate submodles test

--- projet/installDependencies/enterVenv.sh
+++ projet/installDependencies/enterVenv.sh
@@ -16,5 +16,9 @@ echo -e "Installing dependencies \n"
 pip3 install -r ${path}/requirements.txt
 [ $? -ne 0 ] && echo "Could not install all the dependencies. Exiting." && return -1
 
-
+git submodule init
+[ $? -ne 0 ] && echo "Could not init submodules. Exiting." && return -1
+git submodule update
+[ $? -ne 0 ] && echo "Could not update submodules. Exiting." && return -1
+echo "Jobe done! You are in the virtual environnement"
 return 0
