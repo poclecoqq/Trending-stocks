9147aedaa4ee8711646d1a9680204fa1d593e3bf
Pierre-Olivier Cote <po.cote@outlook.com>
2020-04-26 18:14:18 -0400

    removed useless work

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 projet/app.py -y 2016 -d 1 -m 1 -p 90
-python3 projet/app.py -y 2015 -d 1 -m 10 -p 90
-python3 projet/app.py -y 2015 -d 1 -m 7 -p 90
-python3 projet/app.py -y 2015 -d 1 -m 4 -p 90
-python3 projet/app.py -y 2015 -d 1 -m 1 -p 90
